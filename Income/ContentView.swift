54789f7c737f761328c5b4f22cf5509ff26a2632
Kapilesh Rajput <rajputkapilesh@outlook.com>
2025-05-02 19:28:21 +0530

    Initial Commit

---
+++ Income/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Income
+//
+//  Created by Kapilesh Rajput on 02/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Income/IncomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeApp.swift
+//  Income
+//
+//  Created by Kapilesh Rajput on 02/05/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
