c9b40a147ec24dbe986e3e3e8424aef8366c2604
Kapilesh Rajput <rajputkapilesh@outlook.com>
2025-05-03 09:05:57 +0530

    Add BalanceView

--- Income/ContentView.swift
+++ Income/ContentView.swift
@@ -23,8 +23,60 @@ struct ContentView: View {
         ),
     ]
     
+    
+    fileprivate func BalanceView() -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 8)
+                .fill(Color.primaryLightGreen)
+            
+            VStack(alignment: .leading, spacing: 8) {
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("BALANCE")
+                            .font(.caption)
+                            .foregroundStyle(.white)
+                        Text("2")
+                            .font(.system(size: 42, weight: .light))
+                            .foregroundStyle(.white)
+                    }
+                    
+                    Spacer()
+                }
+                .padding(.top)
+                
+                HStack(spacing: 25) {
+                    VStack(alignment: .leading) {
+                        Text("Expense")
+                            .font(.system(size: 15, weight: .semibold))
+                        Text("22")
+                            .font(.system(size: 15, weight: .regular))
+                    }
+                    .foregroundStyle(.white)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Income")
+                            .font(.system(size: 15, weight: .semibold))
+                        Text("22")
+                            .font(.system(size: 15, weight: .regular))
+                    }
+                    .foregroundStyle(.white)
+                }
+                
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+        }
+        .shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: 5)
+        .frame(height: 150)
+        .padding(.horizontal)
+    }
+    
     var body: some View {
         VStack {
+            BalanceView()
+            
             List {
                 ForEach(transactions) { transaction in
                     TransactionView(transaction: transaction)
